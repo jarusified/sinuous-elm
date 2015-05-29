5b36508151c71aaffb9f81d56b7398d7e622b306
= <=>
2015-05-30 01:16:14 +0530

    not rendering red dot

--- sinuous.elm
+++ sinuous.elm
@@ -13,20 +13,27 @@ import Mouse
 import Color exposing (..)
 
 -- model
-main : Signal Element
-main =
-  Signal.map2 scene Mouse.position Window.dimensions
 
+redDots={ pos  = (0,10)
+          ,vel = (-10,0)
+          ,dia = 10
+        }
+
+-- render
 
 scene : (Int,Int) -> (Int,Int) -> Element
 scene (x,y) (w,h) =
-  let (dx,dy) =
-        (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
-  in
-       collage w h
-        [ circle 10
-            |> filled red
-        , circle 13
+  let (dx,dy) = (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+      dot (dia,pos) = circle dia |> filled red |> move pos
+      dots = dot
+ in
+       collage w h dot
+        [  circle 13
             |> filled blue
             |> move (dx, dy)
         ] |> color black
+
+
+main : Signal Element
+main =
+  Signal.map2 scene Mouse.position Window.dimensions
